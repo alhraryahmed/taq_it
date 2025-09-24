81e543f84c62443e379227b77a96ca1ad33ae6df
Ahmed Alhrary <alhraryahmed110@gmail.com>
2025-09-24 13:41:20 +0000

    تحديث doctype taq_data1 + إضافة workflow جديد

---
+++ taq_it/lagna_waqf/doctype/lagnaa_waqf/lagnaa_waqf.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, ahmeed and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class lagnaa_waqf(Document):
+	pass
