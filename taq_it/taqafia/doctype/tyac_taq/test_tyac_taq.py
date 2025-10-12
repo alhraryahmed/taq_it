6b4d3e04191c81152d179bf452fea8102849f9f6
Ahmed Alhrary <alhraryahmed110@gmail.com>
2025-10-12 10:16:24 +0000

    قسم الانشطة والفاعليات

---
+++ taq_it/taqafia/doctype/tyac_taq/test_tyac_taq.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2025, ahmeed and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests import IntegrationTestCase
+
+
+# On IntegrationTestCase, the doctype test records and all
+# link-field test record dependencies are recursively loaded
+# Use these module variables to add/remove to/from that list
+EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+
+
+
+class IntegrationTesttyac_taq(IntegrationTestCase):
+	"""
+	Integration tests for tyac_taq.
+	Use this class for testing interactions between multiple components.
+	"""
+
+	pass
