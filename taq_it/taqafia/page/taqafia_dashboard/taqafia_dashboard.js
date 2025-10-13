37a7ec6e83e61a18ab5c26f9b96e64bcff450937
Ahmed Alhrary <alhraryahmed110@gmail.com>
2025-10-13 14:02:16 +0000

    13-10-2025

---
+++ taq_it/taqafia/page/taqafia_dashboard/taqafia_dashboard.js
@@ -0,0 +1,7 @@
+frappe.pages['taqafia-dashboard'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'إدارة الشؤون الثقافية والدعوية',
+		single_column: true
+	});
+}
